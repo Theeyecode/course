514dd3b6e7afeb368e27735e1df96127fff1731c
Emmanuel Olajubu <emmanuelolajubu90@gmai.com>
2026-01-26 09:30:30 -0500

    custom generic test

--- airbnb/tests/consistent_created_at.sql
+++ airbnb/tests/consistent_created_at.sql
@@ -1,4 +1,4 @@
-SELECT * FROM {{ ref('dim_listings_cleansed') }} AS l
-INNER JOIN {{ ref('fct_reviews') }} AS r
-ON l.listing_id = r.listing_id
+SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }} r
+USING (listing_id)
 WHERE l.created_at > r.review_date
