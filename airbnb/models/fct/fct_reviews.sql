392153463dc740bd4c50f31c5344b8850df6cde3
Emmanuel Olajubu <emmanuelolajubu90@gmai.com>
2026-01-29 23:21:53 -0500

    update fct_review

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -15,9 +15,14 @@ SELECT
 {{ dbt_utils.generate_surrogate_key(['listing_id','review_date', 'reviewer_name', 'review_text']) }} AS review_id,
  *
     FROM src_reviews
-    WHERE reviewer_name is not NULL
-
--- This part tell dbt how to increment
+WHERE review_text is not null
 {% if is_incremental() %}
-    AND review_date > (SELECT max(review_date) from {{ this }})
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
